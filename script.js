5c962ab8d04790f0002dd9acff73d0a2406b34e6
Mooray22 <124084736+Mooray22@users.noreply.github.com>
2023-02-06 23:01:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const $ = document.querySelectorAll.bind(document);
+
+var button = document.getElementById("btn1");
+
+button.addEventListener("click",function()
+{
+    alert("SHUT UP")
+});
+
+
+function changeImage()
+{
+    var img = document.getElementById("image");
+    img.src = "sike.jpg";
+}
+
+
+let color = ["lightblue"];
+var button = document.getElementById("btncolor");
+button.addEventListener("click", function())
+{
+    console.log("test");
+}
